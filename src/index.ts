cb69176854c769b17fc34d521e25f316925d40c3
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-07-11 18:11:52 +0700

    Fetching user data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "John Doe", age: 19 });
-user.on("change", () => console.log("Updated"));
+const user = new User({ id: 1 });
+user.on("change", () => console.log(user));
 
-console.log(user.get("name"));
-user.set({ name: "Johny" });
-console.log(user.get("name"));
+user.fetch();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from "axios";
 import { Eventing } from "./Eventing";
 import { Sync } from "./Sync";
 import { Attribute } from "./Attribute";
@@ -34,5 +35,15 @@ export class User {
     set(update: UserProps): void {
         this.attributes.set(update);
         this.events.trigger("change");
+    }
+
+    fetch(): void {
+        const id = this.attributes.get("id");
+        if (typeof id !== "number")
+            throw new Error("Cannot fetch without an id");
+
+        this.sync.fetch(id).then((response: AxiosResponse): void => {
+            this.set(response.data);
+        });
     }
 }
