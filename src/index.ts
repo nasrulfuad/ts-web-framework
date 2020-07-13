32b35f614a7e1a094c5a2bafeaf37528f2df2fb2
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-07-13 21:21:46 +0700

    Generic user collection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { User } from "./models/User";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const user = User.buildUser({ id: 1 });
-user.on("change", () => console.log(user));
+const collection = new Collection<User, UserProps>(
+    "http://localhost:3000/users",
+    (json: UserProps) => User.buildUser(json)
+);
 
-user.fetch();
+collection.on("change", () => console.log(collection));
+
+collection.fetch();
