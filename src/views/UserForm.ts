f008ffa9f2e03bedbfda66dda9e4f1c2a987cd2e
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-08-20 15:58:19 +0700

    Saving user

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,6 +6,7 @@ export class UserForm extends View<User, UserProps> {
         return {
             "click:.set-age": this.onSetAgeClick,
             "click:.set-name": this.onSetNameClick,
+            "click:.save-model": this.onSaveClick,
         };
     }
 
@@ -21,15 +22,17 @@ export class UserForm extends View<User, UserProps> {
         this.model.setRandomAge();
     };
 
+    onSaveClick = (): void => {
+        this.model.save();
+    };
+
     template(): string {
         return `
             <div>
-              <h1>User Form</h1>
-              <div>User name: ${this.model.get("name")}</div>
-              <div>User age: ${this.model.get("age")}</div>
-              <input class="name" />
+              <input class="name" placeholder="${this.model.get("name")}" />
               <button class="set-name">Click Name</button>
               <button class="set-age">Set Random Age</button>
+              <button class="save-model">Save User</button>
             </div>
         `;
     }
