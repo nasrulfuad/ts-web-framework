93f26a4f911c296ad322cc049229208f1bf5faec
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-08-21 20:49:59 +0700

    Print user element when rendering

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { View } from "./View";
 import { User, UserProps } from "../models/User";
+import { UserShow } from "./UserShow";
+import { UserForm } from "./UserForm";
 
 export class UserEdit extends View<User, UserProps> {
     regionsMap(): { [key: string]: string } {
@@ -9,11 +11,16 @@ export class UserEdit extends View<User, UserProps> {
         };
     }
 
+    onRender(): void {
+        new UserShow(this.regions.userShow, this.model).render();
+        new UserForm(this.regions.userForm, this.model).render();
+    }
+
     template(): string {
         return `
             <div>
-                <div class="user-form"></div>
-                <div class="user-show"></div>
+            <div class="user-show"></div>
+            <div class="user-form"></div>
             </div>
         `;
     }
--- src/views/View.ts
+++ src/views/View.ts
@@ -46,6 +46,8 @@ export abstract class View<T extends Model<K>, K> {
         }
     }
 
+    onRender(): void {}
+
     render(): void {
         this.parent.innerHTML = "";
         const templateElement = document.createElement("template");
@@ -54,6 +56,8 @@ export abstract class View<T extends Model<K>, K> {
 
         this.bindEvents(templateElement.content);
         this.mapRegions(templateElement.content);
+
+        this.onRender();
 
         this.parent.append(templateElement.content);
     }
