f1a842570a65cf3ac0ab2daf5c0e48cbe9563fee
Nasrul Fuad <nasrulfuad08@gmail.com>
2020-08-21 19:47:15 +0700

    UserEdit with region mapping

---
+++ src/views/UserEdit.ts
@@ -0,0 +1,20 @@
+import { View } from "./View";
+import { User, UserProps } from "../models/User";
+
+export class UserEdit extends View<User, UserProps> {
+    regionsMap(): { [key: string]: string } {
+        return {
+            userShow: ".user-show",
+            userForm: ".user-form",
+        };
+    }
+
+    template(): string {
+        return `
+            <div>
+                <div class="user-form"></div>
+                <div class="user-show"></div>
+            </div>
+        `;
+    }
+}
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,17 +1,26 @@
 import { Model } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K> {
+    regions: { [key: string]: Element } = {};
+
     constructor(public parent: Element, public model: T) {
         this.bindModel();
     }
 
-    abstract eventsMap(): { [key: string]: () => void };
     abstract template(): string;
 
     bindModel(): void {
         this.model.on("change", () => {
             this.render();
         });
+    }
+
+    regionsMap(): { [key: string]: string } {
+        return {};
+    }
+
+    eventsMap(): { [key: string]: () => void } {
+        return {};
     }
 
     bindEvents(fragment: DocumentFragment): void {
@@ -24,6 +33,19 @@ export abstract class View<T extends Model<K>, K> {
         }
     }
 
+    mapRegions(fragmanet: DocumentFragment): void {
+        const regionsMap = this.regionsMap();
+
+        for (let key in regionsMap) {
+            const selector = regionsMap[key];
+            const element = fragmanet.querySelector(selector);
+
+            if (element) {
+                this.regions[key] = element;
+            }
+        }
+    }
+
     render(): void {
         this.parent.innerHTML = "";
         const templateElement = document.createElement("template");
@@ -31,6 +53,8 @@ export abstract class View<T extends Model<K>, K> {
         templateElement.innerHTML = this.template();
 
         this.bindEvents(templateElement.content);
+        this.mapRegions(templateElement.content);
+
         this.parent.append(templateElement.content);
     }
 }
